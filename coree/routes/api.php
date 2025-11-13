8e7d00eb101468d434e1469f4c1b2c12ca8c670c
mazdakxezri <mazdakxezri@github.com>
2025-11-13 02:46:17 -0800

    Add missing api.postback.callback route

--- coree/routes/api.php
+++ coree/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Postback\Api\ApiCallbackController;
 
 Route::get('/getWithdrawAndCompletedOffers', [DashboardController::class, 'getWithdrawAndCompletedOffers'])->name('getWithdrawAndCompletedOffers');
+
+// API Postback route for admin panel display
+Route::get('/postback/{network_name}/{secret_key}', [ApiCallbackController::class, 'index'])->name('api.postback.callback');
 
