d71f1fee82aa2977f2c2af4859099e954e9a9174
mazdakxezri <mazdakxezri@github.com>
2025-11-13 05:56:49 -0800

    Register Discord and Steam Socialite providers in AppServiceProvider

--- coree/app/Providers/AppServiceProvider.php
+++ coree/app/Providers/AppServiceProvider.php
@@ -32,5 +32,34 @@ class AppServiceProvider extends ServiceProvider
                 $view->with('pendingWithdrawals', $pendingWithdrawals);
             }
         });
+        
+        // Register Socialite providers
+        $this->bootSocialiteProviders();
+    }
+
+    /**
+     * Boot Socialite providers (Discord, Steam, etc.)
+     */
+    protected function bootSocialiteProviders(): void
+    {
+        $socialite = $this->app->make('Laravel\Socialite\Contracts\Factory');
+        
+        // Discord Provider
+        $socialite->extend('discord', function ($app) use ($socialite) {
+            $config = $app['config']['services.discord'];
+            return $socialite->buildProvider(
+                \SocialiteProviders\Discord\Provider::class,
+                $config
+            );
+        });
+        
+        // Steam Provider  
+        $socialite->extend('steam', function ($app) use ($socialite) {
+            $config = $app['config']['services.steam'];
+            return $socialite->buildProvider(
+                \SocialiteProviders\Steam\Provider::class,
+                $config
+            );
+        });
     }
 }
